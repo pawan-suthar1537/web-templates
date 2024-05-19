ca6c7175b1ec52aacfedcd5c7f4d5c9e7b9c6b77
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-05-19 18:49:38 +0530

    template form added with reactQuill

--- components/dashboard/TemplateForm.jsx
+++ components/dashboard/TemplateForm.jsx
@@ -1,18 +1,105 @@
-import React from 'react'
-import { Input } from '../ui/input'
+import React from "react";
+import { Input } from "../ui/input";
+import { CSSFrameworks, UseCases, Webframeworks } from "@/data";
+import { Label } from "../ui/label";
+import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
+import { Button } from "../ui/button";
+
+const ReactQuill = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
+
+const Modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video", "code-block"],
+    ["clean"],
+    [{ color: [] }],
+  ],
+  clipboard: {
+    matchVisual: false,
+  },
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "color",
+  "code",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "code-block",
+];
 
 const TemplateForm = () => {
   return (
     <>
-    <form action="" className='p-3 space-y-6'>
-      <Input type="text" placeholder="Template Name"/>
-      <div className='grid grid-cols-3 gap-4'>
-        
-
-      </div>
-    </form>
+      <form action="" className="p-3 space-y-6">
+        <Input type="text" placeholder="Template Name" />
+        <div className="grid grid-cols-3 gap-4">
+          <select className="border rounded-md px-3 p-2 w-full">
+            <option value="">Select Framework</option>
+            {Webframeworks.map((f) => (
+              <option key={f} value={f}>
+                {f}
+              </option>
+            ))}
+          </select>
+          <select className="border rounded-md px-3 p-2 w-full">
+            <option value="">Select Css</option>
+            {CSSFrameworks.map((f) => (
+              <option key={f} value={f}>
+                {f}
+              </option>
+            ))}
+          </select>
+          <select className="border rounded-md px-3 p-2 w-full">
+            <option value="">Select UseCases</option>
+            {UseCases.map((f) => (
+              <option key={f} value={f}>
+                {f}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <Input type="url" placeholder="Github Repository Link" />
+          <Input type="url" placeholder="Deployed Link" />
+        </div>
+        <div className="grid w-full items-center gap-2">
+          <Label>Template Images</Label>
+          <Input type="file" />
+          {/* selected images yha bshow krna hai  */}
+        </div>
+        <ReactQuill
+          theme="snow"
+          modules={Modules}
+          formats={formats}
+          placeholder="Describe about your template"
+          className="flex-grow my-3 h-auto"
+        />
+        <Button>
+          Create
+        </Button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default TemplateForm
+export default TemplateForm;
---
+++ data.js
@@ -0,0 +1,43 @@
+export const Webframeworks = [
+  "Next.js",
+  "React",
+  "Vue",
+  "Angular",
+  "Nuxt",
+  "Ember.js",
+  "Svelte",
+  "Backbone.js",
+  "Meteor",
+  "Aurelia",
+  "Polymer",
+  "Mithril",
+];
+
+export const CSSFrameworks = [
+  "Bootstrap",
+  "Foundation",
+  "Bulma",
+  "Materialize",
+  "Semantic UI",
+  "Tailwind CSS",
+  "Pure CSS",
+  "Milligram",
+  "Spectre.css",
+  "Tachyons",
+];
+
+export const UseCases = [
+  "Blog",
+  "CMS",
+  "Documentation",
+  "E-commerce",
+  "Social Media",
+  "Content Management",
+  "Project Management",
+  "Learning Management",
+  "Customer Relationship Management",
+  "Enterprise Resource Planning",
+  "Business Intelligence",
+  "Supply Chain Management",
+  "Human Resource Management",
+];
